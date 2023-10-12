c143645800da39140e65b1219509bcc13928adb5
dipeshbasnet146 <munnabasnet146@gmail.com>
2023-10-12 14:39:50 -0400

    modified main to add capitalization

--- FullNameGenerator/src/main/java/org/example/Main.java
+++ FullNameGenerator/src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+      String message = "Yikes Stripes!";
+        System.out.println(message.charAt(i) );
+
+
+
+        String first = GetStringInput(Scanner, "Enter Your first name: " ).trim();
+         String middle = GetStringInput(Scanner, "Enter Your middle name:" ).trim();
+         String last = GetStringInput(Scanner, "Enter Your last name: " ).trim();
+         String title =  GetStringInput(Scanner, "Enter Your title: " ).trim();
+
+        System.out.println(first + middle + last + title);
+
+
+
+
+
+
+
+
+
+
+        System.out.println( "what is life");
     }
 }
